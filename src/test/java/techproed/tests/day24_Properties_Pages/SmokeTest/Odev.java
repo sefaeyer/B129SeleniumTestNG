9a8d070a10348d886bf2417bec0c8dabaa67b034
Sefa Eyer <eyersefa@gmail.com>
2023-05-03 00:18:44 +0300

    TestNG 02.05.2023

---
+++ src/test/java/techproed/tests/day24_Properties_Pages/SmokeTest/Odev.java
@@ -0,0 +1,13 @@
+package techproed.tests.day24_Properties_Pages.SmokeTest;
+
+public class Odev {
+    /*
+
+Acceptance Criteria:
+Kullanici arac bilgilerini girip CONTINUE RESERVATION butonuna tikladiginda
+Ve giris yapilmadiginda
+Hata mesaji almali : Please first login
+Giris yapildiginda hata mesaji alınmamalı
+
+         */
+}
